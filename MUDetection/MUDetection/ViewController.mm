b376b748a6d0078406078b1b0492f9a0dd1a4552
cmusibowang <286383474@qq.com>
2015-11-26 15:23:48 -0500

    add context of RGimage

--- MUDetection/MUDetection/ViewController.mm
+++ MUDetection/MUDetection/ViewController.mm
@@ -20,6 +20,7 @@ const Scalar GREEN = Scalar(0,255,0);
 {
     UIImageView *liveView_; // Live output from the camera
     CvVideoCamera *videoCamera;
+    cv::Mat RGface;
 }
 
 @end
@@ -31,8 +32,42 @@ const Scalar GREEN = Scalar(0,255,0);
 // Setup view for excuting App
 - (void)viewDidLoad {
     [super viewDidLoad];
+    cv::CascadeClassifier face_cascade;
+    NSString* cascadePath1 = [[NSBundle mainBundle]
+                              pathForResource:@"haarcascade_frontalface_alt" ofType:@"xml"];
+    face_cascade.load([cascadePath1 UTF8String]);
     
-    // Do any additional setup after loading the view, typically from a nib.
+    NSString* imagePath2 = [[NSBundle mainBundle]
+                            pathForResource:@"RGuser" ofType:@"JPG"];
+    UIImage* imgFromUrl2=[[UIImage alloc]initWithContentsOfFile:imagePath2];
+    
+    cv::Mat RGimage,gray;
+    UIImageToMat(imgFromUrl2,RGimage);
+    RGimage=RGimage.t();
+
+    cv::cvtColor(RGimage, gray, CV_BGR2GRAY); // Convert to grayscale
+    cv::Mat im = gray;
+    cv::Mat display_im=RGimage;
+    
+    vector<cv::Rect> faces;
+    //equalizeHist( frame_gray, frame_gray );
+    std::cout<<im.cols<<" "<<im.rows<<std::endl;
+    face_cascade.detectMultiScale( im, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE,cv::Size(50, 100) );
+    std::cout << "Detected " << faces.size() << " faces!!!! " << std::endl;
+    for( int i = 0; i < faces.size(); i++ )
+    {
+        rectangle(display_im, faces[i], YELLOW);
+    }
+    
+    std::cout<<display_im.cols<<display_im.rows;
+    
+//display_im=display_im.t();
+/*
+    Mat faceROI=RGimage(faces[0]);
+    RGface=faceROI;                    ///////////////
+*/
+    //RGface=RGimage2;
+    
     
     // 1. Setup the your OpenCV view, so it takes up the entire App screen......
     int view_width = self.view.frame.size.width;
@@ -41,6 +76,12 @@ const Scalar GREEN = Scalar(0,255,0);
     liveView_ = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, view_offset, view_width, view_height)];
     [self.view addSubview:liveView_]; // Important: add liveView_ as a subview
     liveView_.hidden=false;
+    
+    //liveView_.image=imgFromUrl2;
+  //  liveView_.image=MatToUIImage(display_im);
+    
+        
+        
     videoCamera = [[CvVideoCamera alloc] initWithParentView:liveView_];
     videoCamera.defaultAVCaptureDevicePosition = AVCaptureDevicePositionFront;
     videoCamera.defaultAVCaptureSessionPreset = AVCaptureSessionPreset640x480;
@@ -51,27 +92,28 @@ const Scalar GREEN = Scalar(0,255,0);
     videoCamera.rotateVideo = YES;
     //videoCamera.recordVideo = YES;
     [videoCamera start];
+   
 }
 
 //===============================================================================================
 
 - (void)processImage:(cv::Mat &)image{
+ 
+    
+    
+    //image=RGface;
+    
     
     // You can apply your OpenCV code HERE!!!!!
     // If you want, you can ignore the rest of the code base here, and simply place
     // your OpenCV code here to process images.
     cv::CascadeClassifier face_cascade; // Cascade classifier for detecting the face
-    //cv::CascadeClassifier eye_cascade;
     cv::CascadeClassifier mouth_cascade;
-    
     NSString* cascadePath1 = [[NSBundle mainBundle]
                               pathForResource:@"haarcascade_frontalface_alt" ofType:@"xml"];
     face_cascade.load([cascadePath1 UTF8String]);
-    /*
-    NSString* cascadePath2 = [[NSBundle mainBundle]
-                              pathForResource:@"haarcascade_eye" ofType:@"xml"];
-    eye_cascade.load([cascadePath2 UTF8String]);
-    */
+    
+    
     NSString* cascadePath3 = [[NSBundle mainBundle]
                               pathForResource:@"haarcascade_mcs_mouth" ofType:@"xml"];
     mouth_cascade.load([cascadePath3 UTF8String]);
@@ -108,20 +150,11 @@ const Scalar GREEN = Scalar(0,255,0);
         }
         cv::Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
         rectangle(display_im, faces[i], YELLOW);
+        std::cout<<faces[0].height<<faces[0].height;   ///////////////////////////////////////////////////
         faces[i].y = int(faces[i].y+faces[i].width*0.5);
         faces[i].height = int(faces[i].height*0.5);
         Mat faceROI = frame_gray( faces[i] );
-        /*
-        vector<cv::Rect> eyes;
-        eye_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CV_HAAR_SCALE_IMAGE, cv::Size(5, 20) );
-        std::cout << "Detected " << eyes.size() << " eyes!!!! " << std::endl;
-        for( int j = 0; j < eyes.size(); j++ )
-        {
-            cv::Point center( faces[i].x + eyes[j].x + eyes[j].width*0.5, faces[i].y + eyes[j].y + eyes[j].height*0.5 );
-            int radius = cvRound( (eyes[j].width + eyes[i].height)*0.25 );
-            circle( display_im, center, radius, GREEN, 4, 8, 0 );
-        }
-        */
+
         vector<cv::Rect> mouths;
         mouth_cascade.detectMultiScale( faceROI, mouths, 1.1, 2, 0 |CV_HAAR_SCALE_IMAGE, cv::Size(30, 50) );
         std::cout << "Detected " << mouths.size() << " mouths!!!! " << std::endl;
@@ -129,7 +162,6 @@ const Scalar GREEN = Scalar(0,255,0);
         {
             cv::Point p1( faces[i].x + mouths[0].x , faces[i].y + mouths[0].y );
             cv::Point p2( faces[i].x + mouths[0].x + mouths[0].width, faces[i].y + mouths[0].y + mouths[0].height);
-            
             /*
             int radius = cvRound( (mouths[j].width + mouths[i].height)*0.25 );
             circle( display_im, center, radius, GREEN, 4, 8, 0 );
@@ -141,6 +173,8 @@ const Scalar GREEN = Scalar(0,255,0);
     rectangle(display_im, mainFace, RED);
     
     image =display_im;
+ //   image=RGface;
+    
 
 
     //  [takephotoButton_ setHidden:true]; [goliveButton_ setHidden:false]; // Switch visibility of buttons
